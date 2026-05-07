e3150df526a318766b2681324b2fd24b2ab4f94b
Bojan Vrhovnik <bojan@vrhovnik.net>
2026-05-07 10:30:50 +0200

    Add initial project structure and configuration files for AI evaluation agents

---
+++ src/EvaluationSLN/Eval.Agent/Program.cs
@@ -0,0 +1,22 @@
+﻿using Azure.AI.Projects;
+using Azure.Identity;
+using Microsoft.Agents.AI;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("[grey]MAF Agent for evaluation[/]");
+
+var endpoint = Environment.GetEnvironmentVariable("ENDPOINT");
+ArgumentException.ThrowIfNullOrEmpty(endpoint, "Please set the ENDPOINT environment variable to your Azure OpenAI endpoint URL.");
+var deploymentName = Environment.GetEnvironmentVariable("DEPLOYMENT_NAME");
+ArgumentException.ThrowIfNullOrEmpty(deploymentName, "Please set the DEPLOYMENT_NAME environment variable to your Azure OpenAI endpoint URL.");
+
+AIAgent agent = new AIProjectClient(new Uri(endpoint), new DefaultAzureCredential())
+    .AsAIAgent(
+        model: deploymentName,
+        instructions: "You are a friendly assistant. Keep your answers brief.",
+        name: "SimpleAgent");
+var question = AnsiConsole.Ask<string>("Ask your question",
+    "What is the 2nd largest city in United States by population size?");
+AnsiConsole.MarkupLine("[green]Question:[/]" + question);
+var answer = await agent.RunAsync(question);
+AnsiConsole.MarkupLine("[green]Answer:[/]" + answer);
---
+++ src/EvaluationSLN/Eval.FoundryAgent/Program.cs
@@ -0,0 +1,30 @@
+﻿using Azure.AI.Projects;
+using Azure.AI.Projects.Agents;
+using Azure.Identity;
+using Spectre.Console;
+
+AnsiConsole.MarkupLine("[grey]Foundry Agent for evaluation[/]");
+
+#region Environment variables
+
+var endpoint = Environment.GetEnvironmentVariable("ENDPOINT");
+ArgumentException.ThrowIfNullOrEmpty(endpoint,
+    "Please set the ENDPOINT environment variable to your Azure OpenAI endpoint URL.");
+var agentName = Environment.GetEnvironmentVariable("AGENT_NAME");
+ArgumentException.ThrowIfNullOrEmpty(agentName,
+    "Please set the AGENT_NAME environment variable to your Azure OpenAI endpoint URL.");
+
+#endregion
+
+var aiProjectClient = new AIProjectClient(new Uri(endpoint), new DefaultAzureCredential());
+
+ProjectsAgentRecord defaultAgent = await aiProjectClient.AgentAdministrationClient.GetAgentAsync(agentName);
+var agent = aiProjectClient.AsAIAgent(defaultAgent);
+AnsiConsole.MarkupLine(
+    $"[green]Successfully retrieved agent with name {agentName} and id {agent.Id}[/]. Asking question now.");
+var question = AnsiConsole.Ask<string>("Ask your question",
+    "What is the 2nd largest city in United States by population size?");
+AnsiConsole.MarkupLine("[green]Question:[/]" + question);
+var agentMessage = await agent.RunAsync(question);
+AnsiConsole.MarkupLine("[green]Answer:[/]");
+AnsiConsole.WriteLine(agentMessage.Text);
