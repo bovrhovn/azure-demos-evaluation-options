7ef0fd42e71bdcd4a3341b13376aef12d6c14c57
Bojan Vrhovnik <bojan@vrhovnik.net>
2026-05-08 08:04:01 +0200

    Add EvaluatorsCatalog project and implement evaluator tests for Azure AI

---
+++ src/EvaluationSLN/Eval.BuiltIn.EvaluatorsCatalog/EvaluatorsCatalogTests.cs
@@ -0,0 +1,78 @@
+﻿using System.ClientModel;
+using Azure.AI.Projects;
+using Azure.AI.Projects.Evaluation;
+using Azure.Identity;
+using Eval.Tests.Base;
+using OpenAI.Evals;
+
+namespace Eval.BuiltIn.EvaluatorsCatalog;
+
+public class EvaluatorsCatalogTests
+{
+    private AIProjectClient projectClient;
+    private EvaluationClient evaluationClient;
+
+    [SetUp]
+    public void Setup()
+    {
+        projectClient = new(new Uri(Environment.GetEnvironmentVariable("ENDPOINT") ?? 
+                                    "https://ai-common.services.ai.azure.com/api/projects/agentic-ai-project"),
+            new DefaultAzureCredential());
+        evaluationClient = projectClient.ProjectOpenAIClient.GetEvaluationClient();
+    }
+
+    [Test]
+    public async Task TestEvaluatorsCatalog()
+    {
+        Console.WriteLine("Creating prompt-based evaluator.");
+        EvaluatorVersion promptEvaluator = await projectClient.Evaluators.CreateVersionAsync(
+            name: "myCustomEvaluatorPromptBased",
+            evaluatorVersion: EvaluatorsTestExtensions.GetPromptEvaluatorVersion()
+        );
+        EvaluatorsTestExtensions.DisplayEvaluatorVersion(promptEvaluator);
+        Console.WriteLine("Creating code-based evaluator.");
+        EvaluatorVersion codeEvaluator = await projectClient.Evaluators.CreateVersionAsync(
+            name: "myCustomEvaluatorCodeBased",
+            evaluatorVersion: EvaluatorsTestExtensions.GetCodeEvaluatorVersion()
+        );
+        EvaluatorsTestExtensions.DisplayEvaluatorVersion(codeEvaluator);
+        Console.WriteLine("Get code-based evaluator.");
+        EvaluatorVersion codeEvaluatorLatest = await projectClient.Evaluators.GetVersionAsync(name: codeEvaluator.Name, version: codeEvaluator.Version);
+        EvaluatorsTestExtensions.DisplayEvaluatorVersion(codeEvaluatorLatest);
+        Console.WriteLine("Get prompt-based evaluator.");
+        EvaluatorVersion promptEvaluatorLatest = await projectClient.Evaluators.GetVersionAsync(name: promptEvaluator.Name, version: promptEvaluator.Version);
+        EvaluatorsTestExtensions.DisplayEvaluatorVersion(promptEvaluatorLatest);
+        
+        Console.WriteLine("Updating code-based evaluator.");
+        var evaluatorVersionUpdate = BinaryData.FromObjectAsJson(
+            new
+            {
+                categories = new[] { EvaluatorCategory.Quality.ToString() },
+                display_name = "my_custom_evaluator_updated",
+                description = "Custom evaluator description changed"
+            }
+        );
+        using var evaluatorVersionUpdateContent = BinaryContent.Create(evaluatorVersionUpdate);
+        var response = await projectClient.Evaluators.UpdateVersionAsync(
+            name: codeEvaluator.Name,
+            version: codeEvaluator.Version,
+            content: evaluatorVersionUpdateContent
+        );
+        EvaluatorVersion updatedEvaluator = ClientResult.FromValue((EvaluatorVersion)response, response.GetRawResponse());
+        EvaluatorsTestExtensions.DisplayEvaluatorVersion(updatedEvaluator);
+        Console.WriteLine("Listing built-in evaluators.");
+        await foreach (var evaluator in projectClient.Evaluators.GetLatestVersionsAsync(type: ListVersionsRequestType.BuiltIn))
+        {
+            EvaluatorsTestExtensions.DisplayEvaluatorVersion(evaluator);
+        }
+        Console.WriteLine("Listing custom evaluators.");
+        await foreach (EvaluatorVersion evaluator in projectClient.Evaluators.GetLatestVersionsAsync(type: ListVersionsRequestType.Custom))
+        {
+            EvaluatorsTestExtensions.DisplayEvaluatorVersion(evaluator);
+        }
+        Assert.Pass("Evaluators catalog test passed.");
+        //cleanup
+        // await projectClient.Evaluators.DeleteVersionAsync(name: promptEvaluatorLatest.Name, version: promptEvaluatorLatest.Version);
+        // await projectClient.Evaluators.DeleteVersionAsync(name: codeEvaluatorLatest.Name, version: codeEvaluatorLatest.Version);
+    }
+}
---
+++ src/EvaluationSLN/Eval.Tests.Base/EvaluatorsTestExtensions.cs
@@ -0,0 +1,132 @@
+﻿using Azure.AI.Projects;
+using Azure.AI.Projects.Evaluation;
+
+namespace Eval.Tests.Base;
+
+public class EvaluatorsTestExtensions
+{
+    public static EvaluatorVersion GetPromptEvaluatorVersion()
+    {
+        EvaluatorMetric metric = new()
+        {
+            Type = EvaluatorMetricType.Ordinal,
+            DesirableDirection = EvaluatorMetricDirection.Increase,
+            MinValue = 1,
+            MaxValue = 5
+        };
+        return new(
+            categories: [EvaluatorCategory.Quality],
+            definition: new PromptBasedEvaluatorDefinition(
+                promptText: """
+                    You are an evaluator.
+                    Rate the GROUNDEDNESS (factual correctness without unsupported claims) of the system response to the customer query.
+
+                    Scoring (1–5):
+                    1 = Mostly fabricated/incorrect
+                    2 = Many unsupported claims
+                    3 = Mixed: some facts but notable errors/guesses
+                    4 = Mostly factual; minor issues
+                    5 = Fully factual; no unsupported claims
+
+                    Return ONLY a single integer 1–5 as score in valid json response e.g {\"score\": int}.
+
+                    Query:
+                    {query}
+
+                    Response:
+                    {response}
+                    """,
+                initParameters: BinaryData.FromObjectAsJson(
+                    new
+                    {
+                        type = "object",
+                        properties = new
+                        {
+                            deployment_name = new { type = "string" },
+                            threshold = new { rtpe = "number" },
+                        },
+                        required = new[] { "deployment_name", "threshold" }
+                    }
+                ),
+                dataSchema: BinaryData.FromObjectAsJson(
+                    new
+                    {
+                        type = "object",
+                        properties = new
+                        {
+                            query = new { type = "string" },
+                            response = new { type = "string" }
+                        }
+                    }
+                ),
+                metrics: new Dictionary<string, EvaluatorMetric> { { "score", metric } }
+            ),
+            evaluatorType: EvaluatorType.Custom
+        )
+        {
+            DisplayName = "my_custom_evaluator",
+            Description = "Custom evaluator to detect violent content",
+        };
+    }
+    
+    public static EvaluatorVersion GetCodeEvaluatorVersion()
+    {
+        EvaluatorMetric resultMetric = new()
+        {
+            Type = EvaluatorMetricType.Ordinal,
+            DesirableDirection = EvaluatorMetricDirection.Increase,
+            MinValue = 0,
+            MaxValue = 5
+        };
+        EvaluatorVersion evaluatorVersion = new(
+            categories: [EvaluatorCategory.Quality],
+            definition: new CodeBasedEvaluatorDefinition(
+                codeText: "def grade(sample, item):\n    return 1.0",
+                initParameters: BinaryData.FromObjectAsJson(
+                    new
+                    {
+                        type = "object",
+                        properties = new
+                        {
+                            deployment_name = new { type = "string" },
+                        },
+                        required = new[] { "deployment_name" },
+                    }
+                ),
+                dataSchema: BinaryData.FromObjectAsJson(
+                    new
+                    {
+                        type = "object",
+                        properties = new
+                        {
+                            item = new { type = "string" },
+                            response = new { type = "string" }
+                        },
+                        required = new[] { "query", "response" },
+                    }
+                ),
+                metrics: new Dictionary<string, EvaluatorMetric> {
+                    { "result", resultMetric }
+                }
+            ),
+            evaluatorType: EvaluatorType.Custom
+        )
+        {
+            DisplayName = "my_custom_evaluator",
+            Description = "Custom evaluator to detect violent content",
+        };
+        return evaluatorVersion;
+    }
+
+    public static void DisplayEvaluatorVersion(EvaluatorVersion evaluator)
+    {
+        Console.WriteLine($"Evaluator ID: {evaluator.Id}");
+        Console.WriteLine($"    Name: {evaluator.Name}");
+        Console.WriteLine($"    Version: {evaluator.Version}");
+        Console.WriteLine("     Categories:");
+        foreach (EvaluatorCategory category in evaluator.Categories)
+        {
+            Console.WriteLine("         - ${category}");
+        }
+    }
+}
