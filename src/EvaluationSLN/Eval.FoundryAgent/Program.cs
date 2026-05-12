195a7f05f9528e9706148f576cf843616cf58ab9
Bojan Vrhovnik <bojan@vrhovnik.net>
2026-05-12 10:48:06 +0200

    refactor: Update environment variable handling and integrate Azure OpenAI chat client to handle Azure Evals

--- src/EvaluationSLN/Eval.FoundryAgent/Program.cs
+++ src/EvaluationSLN/Eval.FoundryAgent/Program.cs
@@ -1,7 +1,9 @@
-﻿using Azure.AI.Projects;
+﻿using Azure.AI.OpenAI;
+using Azure.AI.Projects;
 using Azure.AI.Projects.Agents;
 using Azure.Identity;
 using Microsoft.Agents.AI;
+using Microsoft.Extensions.AI;
 using Microsoft.Extensions.AI.Evaluation;
 using Microsoft.Extensions.AI.Evaluation.Quality;
 using Spectre.Console;
@@ -10,20 +12,28 @@ AnsiConsole.MarkupLine("[grey]Foundry Agent for evaluation[/]");
 
 #region Environment variables
 
-var endpoint = Environment.GetEnvironmentVariable("ENDPOINT");
-ArgumentException.ThrowIfNullOrEmpty(endpoint,
-    "Please set the ENDPOINT environment variable to your Azure OpenAI endpoint URL.");
+var azureFoundryEndpoint = Environment.GetEnvironmentVariable("FOUNDRY_ENDPOINT");
+ArgumentException.ThrowIfNullOrEmpty(azureFoundryEndpoint,
+    "Please set the FOUNDRY_ENDPOINT environment variable to your Azure OpenAI endpoint URL.");
+var azureOpenAIEndpoint = Environment.GetEnvironmentVariable("OPENAI_ENDPOINT");
+ArgumentException.ThrowIfNullOrEmpty(azureOpenAIEndpoint,
+    "Please set the OPENAI_ENDPOINT environment variable to your Azure OpenAI endpoint URL.");
 var agentName = Environment.GetEnvironmentVariable("AGENT_NAME");
 ArgumentException.ThrowIfNullOrEmpty(agentName,
     "Please set the AGENT_NAME environment variable to your Azure OpenAI endpoint URL.");
+var deploymentName = Environment.GetEnvironmentVariable("DEPLOYMENT_NAME");
+ArgumentException.ThrowIfNullOrEmpty(deploymentName,
+    "Please set the DEPLOYMENT_NAME environment variable to your Azure OpenAI endpoint URL.");
 
 #endregion
 
-var aiProjectClient = new AIProjectClient(new Uri(endpoint), new DefaultAzureCredential());
-ProjectsAgentRecord defaultAgent = await aiProjectClient.AgentAdministrationClient.GetAgentAsync(agentName);
+var aiProjectClient = new AIProjectClient(new Uri(azureFoundryEndpoint), new DefaultAzureCredential());
+ProjectsAgentRecord defaultAgent = await aiProjectClient.AgentAdministrationClient
+    .GetAgentAsync(agentName);
 var agent = aiProjectClient.AsAIAgent(defaultAgent);
 AnsiConsole.MarkupLine(
-    $"[green]Successfully retrieved agent with name {agentName} and id {agent.Id}[/]. Asking question now.");
+    $"[green]Successfully retrieved agent with name {agentName} and id {agent.Id}[/]. " +
+    $"Asking question now.");
 var question = AnsiConsole.Ask<string>("Ask your question",
     "What is the 2nd largest city in Poland by population size?");
 AnsiConsole.MarkupLine("[green]Question:[/]" + question);
@@ -54,11 +64,20 @@ if (continueWith)
 
 continueWith = AnsiConsole.Ask("Continue with Azure Coherence evaluator?",
     true);
+var client =
+    new ChatClientBuilder(
+        new AzureOpenAIClient(new Uri(azureOpenAIEndpoint), new DefaultAzureCredential())
+            .GetChatClient(deploymentName)
+            .AsIChatClient())
+        .Build();
+var chatConfiguration = new ChatConfiguration(client);
+AnsiConsole.MarkupLine($"[grey]Chat client with Azure OpenAI model {deploymentName} created[/]");
 if (continueWith)
 {
     //check with Azure Foundry evaluator 
     var coherenceEvaluator = new CoherenceEvaluator();
-    var result = await coherenceEvaluator.EvaluateAsync(question, agentMessage.Text);
+    var result = await coherenceEvaluator.EvaluateAsync(question, agentMessage.Text,
+        chatConfiguration);
     foreach (var keyValuePair in result.Metrics)
     {
         AnsiConsole.WriteLine($"Metric: {keyValuePair.Key}");
@@ -76,7 +95,8 @@ if (continueWith)
         new FluencyEvaluator()
     );
 
-    var result = await evaluator.EvaluateAsync(question, agentMessage.Text);
+    var result = await evaluator.EvaluateAsync(question, agentMessage.Text, 
+        chatConfiguration);
     foreach (var keyValuePair in result.Metrics)
     {
         AnsiConsole.WriteLine($"Metric: {keyValuePair.Key}");
